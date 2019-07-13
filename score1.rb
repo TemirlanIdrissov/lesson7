982d3ceba096ff829245ce25b8f622b28c19a5e1
TemirlanIdrissov <51860679+TemirlanIdrissov@users.noreply.github.com>
2019-07-13 17:34:45 +0600

    Update score1.rb

--- score1.rb
+++ score1.rb
@@ -1,18 +1,18 @@
-def score_throws(score)
-	array = []
-	score.each do |i|
-    if i < 5
-    array << 10.to_f
+def score_throws(radiuses)
+  return 0 if radiuses.empty?
 
-    elsif i >= 5 && i <= 10
-    array << 5.to_f
+    array = []
+    radiuses.each do |r|
+    radius = r.to_f
 
-    else 
-    array << 0.to_f
+      array << 0 if radius > 10
+      array << 10 if radius < 5
+      array << 5 if radius >= 5 && radius <= 10
+    end
 
-    end 
-  end
-      puts "Score: #{score} => #{array.sum}"
+        score = array.sum
+        score += 100 if radiuses.all? {|r| r < 5}
+        puts "Score: #{radiuses} => #{score}"
 
 end
 
